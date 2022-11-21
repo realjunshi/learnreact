788d71a96f202537bf6b5c53585d8409006e302a
hejianglong <hejianglong@changba.com>
2022-11-21 23:35:40 +0800

    useRef 用来获取DOM元素 useRef 用来保存变量

--- src/App.js
+++ src/App.js
@@ -3,22 +3,15 @@ import './App.css';
 import React, { useEffect, useRef, useState} from "react";
 
 function App() {
-    // setState执行渲染，如果是对象，里面的值必须是一个新对象
-    const [count, setCount] = useState(0)
-    const [age, setAge] = useState(0)
+    const inputEl = useRef(null)
+    const onButtonClick = () => {
+        inputEl.current.focus();
+    }
 
-    useEffect(() => {
-        console.log('我执行了count=', count)
-        return () => {
-            console.log('componentWillUnmount')
-        }
-    }, [count])
     return (
       <>
-          <h2>{count}</h2>
-          <button onClick={onClick => (setCount(count + 1))}>改变-age</button>
-          <h2>effect</h2>
-          <button onClick={onClick => {setAge(age + 1)}}>改变-count</button>
+          <input ref={inputEl} type="text" />
+          <button onClick={onButtonClick}>Focus the input</button>
       </>
     );
 }
