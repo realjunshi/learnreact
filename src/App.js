641966293833c3b3ac45821ff01da60956f881a0
hejianglong <hejianglong@changba.com>
2022-11-22 17:00:32 +0800

    异步的方式能多次渲染

--- src/App.js
+++ src/App.js
@@ -4,24 +4,34 @@ import React, { useEffect, useRef, useState} from "react";
 
 
 function App() {
+    const initCount = 0;
     const [count, setCount] = useState(0);
 
-    function clickHandler(){
-        setCount(count + 1);
-    }
-
-    const clickHandler2 = () => {
+    const clickHandler = () => {
         setCount(count + 1)
     }
     return (
         <>
-            <div onClick={clickHandler}>
+            {/*<div onClick={clickHandler}>*/}
+            <div>
                 计数: {count}
+                <button onClick={() => {setCount(initCount)}}>init</button>
+                <button onClick={() => {setCount(count + 1)}}>+1</button>
+                <button onClick={() => {setCount(count - 1)}}>-1</button>
+                <button onClick={() => {
+                    // let num = 0;
+                    for (let i = 0;i < 3;i++){
+                        // num += 1
+                        // 异步返回值累加
+                        // setCount(count => count + 1)
+                        setCount(count => {
+                            return count + 1
+                        })
+                    }
+                    // setCount(count + num)
+                }}>test click</button>
             </div>
 
-            <div onClick={clickHandler2}>
-                累计计数器: {count}
-            </div>
         </>
     );
 }
