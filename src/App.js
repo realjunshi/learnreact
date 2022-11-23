b60e5d2a0843d01405c6d57b23786e541900763b
hejianglong <hejianglong@changba.com>
2022-11-23 12:02:40 +0800

    useReducer 处理逻辑类似的功能

--- src/App.js
+++ src/App.js
@@ -1,93 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useEffect, useRef, useState} from "react";
+import React, {useContext, useReducer} from "react";
 
+
+// function reducer(state, action){
+//     switch (action) {
+//         case 'xx':
+//             return xxx;
+//         case 'xxx':
+//             return xxxx;
+//         default:
+//             return xxxxx;
+//     }
+// }
+//
+// function Component(){
+//     // 声明一个变量xxx，以及对应修改的xxx的dispatch
+//     //将事件处理函数reducer和默认值initialValue作为参数传递给useReducer
+//     const [xxx, dispatch] = useReducer(reducer, 0);
+//
+//     // 若想修改xxx的值，通过dispatch来修改
+//     dispatch('xxx');
+// }
+
+function reducer(state, action){
+    switch (action) {
+        case 'add':
+            return state + 1
+        case 'sub':
+            return state - 1
+        case 'mul':
+            return state * 2
+        default:
+            console.log('what?')
+            return state
+    }
+}
 
 function App() {
-    const [str, setStr] = useState('');
-    const [arr, setArr] = useState(['react', 'koa'])
 
-    // 获取输入的数据
-    const inputChangeHandler = (eve) => {
-        setStr(eve.target.value)
-    }
+    const [count, dispatch] = useReducer(reducer, 0);
 
-    // 添加至头
-    const addHeadHandler = (eve) => {
-        setArr([str, ...arr]);
-        setStr("")
-    }
-
-    // 添加至尾
-    const addEndHandler = (eve) => {
-        setArr([ ...arr, str]);
-        setStr("")
-    }
-
-    // 删除头
-    const delHeadHandler = (eve) => {
-        let new_arr = [...arr]
-        new_arr.shift();
-        setArr(new_arr)
-    }
-    // 删除尾
-    const delEndHandler = (eve) => {
-        let new_arr = [...arr];
-        new_arr.pop();
-        setArr(new_arr)
-    }
-
-    const delByIndex = (eve) => {
-        let index = eve.target.attributes.index.value;
-        let new_arr = [...arr]
-        new_arr.splice(index, 1)
-        setArr(new_arr)
-    }
-
-    // 测试渲染逻辑
-    const testRendering = () => {
-        let str = 'a';
-        console.log(Object.is(str, 'a'));
-
-        let str2 = '18';
-        console.log(Object.is(str2,  18));
-
-        let obj = {name: 'a'}
-        console.log(Object.is(obj, {name: 'a'}));
-
-        let obj2 = {name: 'a'}
-        let a = obj2;
-        let b = obj2;
-        console.log(Object.is(a, b))
-    }
 
     return (
-        <>
-            <div>
-                <input type='text' value={str} onChange={inputChangeHandler}/>
-                <button onClick={addHeadHandler}>添加至头</button>
-                <button onClick={addEndHandler}>添加至尾</button>
-                <button onClick={delHeadHandler}>从头删除1项</button>
-                <button onClick={delEndHandler}>从尾删除1项</button>
-            </div>
-
-            <ul>
-                {arr.map(
-                    (item, index) => {
-                        return <li key={`item${index}`}>
-                            学习{index} - {item}
-                            <span index={index} onClick={delByIndex} style={{ cursor: 'pointer' }}>
-                                删除
-                            </span>
-                        </li>
-                    }
-                )}
-            </ul>
-
-            <div>
-                <button onClick={testRendering}>测试渲染</button>
-            </div>
-        </>
+        <div>
+            {count}
+            <button onClick={() => {dispatch('add')}}>add</button>
+            <button onClick={() => {dispatch('sub')}}>sub</button>
+            <button onClick={() => {dispatch('mul')}}>mul</button>
+        </div>
     );
 }
 
